3e5a575869a2e640208664d1988a5d5ae419d01e
Pedro Vitor <pedrocavt@gmail.com>
2020-08-01 19:15:03 -0300

    sixth

--- index.php
+++ index.php
@@ -3,15 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Loja</title>
+    <title>Luanna Colchões</title>
     <!-- CSS only -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
-<style>
-    .navbar{
-        padding-bottom: 0px;
-    }
-</style>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>    
 <?php 
@@ -35,13 +31,33 @@
         ?>
 
             <div class="col-sm-3">
-                <img src="img/<?php echo $exibir['foto1']; ?>" class="img-responsive" style="width:100%; height:60%;" >
+                <img src="img/<?php echo $exibir['imagem_produto']; ?>" class="img-responsive" style="width:100%; height:60%;" >
                 <div>
-                    <h4><?php echo $exibir['produto']; ?></h4>
+                    <h4><?php echo mb_strimwidth($exibir['nome_produto'],0,22,'...'); ?></h4>
                 </div>
                 <div>
-                    <h5>R$ <?php echo $exibir['preco']; ?></h5>
+                    <h5>R$ <?php echo number_format($exibir['preco_produto'],2,',','.'); ?></h5>
                 </div>
+
+                <div class="">
+                    <button class="btn btn-lg" style="background-color:#46010a;color:white">
+                    <i class="fas fa-info-circle"></i>&nbsp;Detalhes
+                    </button>
+
+                        <?php if ($exibir['quantidade_produto'] > 0) { ?>
+
+                    <button class="btn btn-lg" style="background-color:#46010a;color:white;float:right">
+                    <i class="fas fa-shopping-cart"></i>&nbsp;Comprar
+                    </button>
+                        <?php } else{ ?>
+
+                    <button class="btn btn-lg" style="background-color:#46010a;color:white;float:right">
+                    <i class="fas fa-exclamation-triangle"></i>&nbsp;Indisponivel
+                    </button>
+                        <?php } ?>
+                       
+                </div>
+
             </div>
             
             <?php } ?>
